2bb9375237de3d9a7d8224084cecd1cc63e3530c
markusritschel <git@markusritschel.de>
2024-03-03 04:55:39 +0100

    Add HistoryAccessor class to xarray.py

--- src/my_code_base/core/xarray.py
+++ src/my_code_base/core/xarray.py
@@ -5,5 +5,25 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
 import logging
+import xarray as xr
 
 log = logging.getLogger(__name__)
+
+
+
+@xr.register_dataset_accessor("history")
+class HistoryAccessor:
+    def __init__(self, xr_obj):
+        self._obj = xr_obj
+        self._ensure_history()
+
+    def _ensure_history(self):
+        if 'history' not in self._obj.attrs.keys():
+            self._obj.attrs['history'] = ""
+
+    def add(self, msg):
+        """Add an entry to the history."""
+        self._obj.attrs['history'] += msg
+        log.debug("Wrote '%s' to history", msg)
+        return
+
