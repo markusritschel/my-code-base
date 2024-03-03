47d30e8a1381e407fb6b45555174ce56c025402b
markusritschel <git@markusritschel.de>
2024-03-03 04:43:03 +0100

    Add adjust_lons function for correct east degrees

--- src/my_code_base/esd/coordinates.py
+++ src/my_code_base/esd/coordinates.py
@@ -5,7 +5,31 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
 import logging
-
+import xarray as xr
 
 log = logging.getLogger(__name__)
 
+
+
+def adjust_lons(ds, lon_name="lon"):
+    """Adjust longitude values to make sure they are in the range between -180 and 180
+    """
+    ds["_longitude_adjusted"] = xr.where(
+        ds[lon_name] > 180, ds[lon_name] - 360, ds[lon_name]
+    )
+
+    # TODO: use or discard the following?
+    attributes = ds[lon_name].attrs.copy()
+
+    # reassign the new coordinates to as the main lon coordinates
+    # and sort DataArray using new coordinate values
+    ds = (
+        ds.swap_dims({lon_name: "_longitude_adjusted"})
+        .sel(**{"_longitude_adjusted": sorted(ds._longitude_adjusted)})
+        .drop(lon_name)
+    )
+
+    ds = ds.rename({"_longitude_adjusted": lon_name})
+    ds[lon_name].attrs['units'] = 'degrees_east'
+    ds[lon_name].attrs['standard_name'] = 'longitude'
+    return ds
