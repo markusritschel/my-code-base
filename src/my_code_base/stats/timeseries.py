54094752cc36a7fe540734a76e164af68da16259
markusritschel <git@markusritschel.de>
2024-03-03 04:17:17 +0100

    Add stats/timeseries.py

---
+++ src/my_code_base/stats/timeseries.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python3
+#
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-03
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
