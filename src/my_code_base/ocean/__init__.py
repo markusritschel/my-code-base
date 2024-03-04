f9a62adedc7ee271807226d8de796cd35086c71e
markusritschel <git@markusritschel.de>
2024-03-04 20:15:18 +0100

    Add module docstring

--- src/my_code_base/ocean/__init__.py
+++ src/my_code_base/ocean/__init__.py
@@ -4,6 +4,13 @@
 # Date:   2024-03-03
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
+"""This module provides routines for tackling oceanic biogeochemistry related tasks.
+
+- temperature and salinity normalization
+- temperature decomposition
+- fgCO2 <-> pCO2 and other conversions
+# TODO: check content of `oceanpack` package and move/copy to `ocean_bgc` if necessary
+"""
 import logging
 
 
