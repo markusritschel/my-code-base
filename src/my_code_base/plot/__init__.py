b1e9bb316b74774243126ee3b3ee762941905964
markusritschel <git@markusritschel.de>
2024-03-04 18:23:27 +0100

    Create plot module

---
+++ src/my_code_base/plot/__init__.py
@@ -0,0 +1,13 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-04
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+"""This module contains various helper functions for plotting.
+"""
+import logging
+
+
+log = logging.getLogger(__name__)
+
