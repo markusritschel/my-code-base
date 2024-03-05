4b28042f0625a0a0e1ed016ad36c80b62be61755
markusritschel <git@markusritschel.de>
2024-03-05 21:08:59 +0100

    Add tests for SouthPolarStereo projection

--- tests/test_plot_maps.py
+++ tests/test_plot_maps.py
@@ -17,9 +17,19 @@ def ax_northpolar(request):
     return ax
 
 
-def test_projection_class(ax_northpolar):
+@pytest.fixture(scope="module")
+def ax_southpolar(request):
+    ax = plt.subplot(projection=ccrs.SouthPolarStereo())
+    return ax
+
+
+def test_projection_class(ax_northpolar, ax_southpolar):
     proj_type = type(ax_northpolar._projection_init[1]['projection'])
     assert proj_type == ccrs.NorthPolarStereo, "ax should be instance of ccrs.NorthPolarStereo"
     assert ax_northpolar.geomap._projection == ccrs.NorthPolarStereo, "ax should be instance of ccrs.NorthPolarStereo"
-    
+    assert ax_southpolar.geomap._projection == ccrs.SouthPolarStereo, "ax should be instance of ccrs.SouthPolarStereo"
+
     assert ax_northpolar.polar._pole == 'north', "Expected `north` as pole name"
+    assert ax_southpolar.polar._pole == 'south', "Expected `south` as pole name"
+
+
