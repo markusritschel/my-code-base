5a4cb828646d6c8bf6afa1cca2733204dd647b62
markusritschel <git@markusritschel.de>
2024-03-05 19:44:19 +0100

    Add default methods to GeoAxesAccessor ABC class

--- src/my_code_base/plot/maps.py
+++ src/my_code_base/plot/maps.py
@@ -16,3 +16,21 @@ class GeoAxesAccessor(ABC):
         log.debug('Initialize accessor')
         self.geo_axes = ax
 
+    def add_ocean(self, **kwargs):
+        log.debug('Add ocean to axis')
+        kwargs.setdefault('zorder', 0)
+        self.geo_axes.add_feature(cartopy.feature.OCEAN, **kwargs)
+
+    def add_land(self, **kwargs):
+        log.debug('Add land to axis')
+        kwargs.setdefault('zorder', 2)
+        self.geo_axes.add_feature(cartopy.feature.LAND, **kwargs)
+
+    def add_coastlines(self, *args, **kwargs):
+        log.debug('Add coastlines to axis')
+        kwargs.setdefault('zorder', 3)
+        self.geo_axes.coastlines(*args, **kwargs)
+
+    def set_extent(self, extent, crs=cartopy.crs.PlateCarree()):
+        log.debug('Set axis extent to %s', extent)
+        self.geo_axes.set_extent(extent, crs)
