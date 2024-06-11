9e2e8c1a92db0681ac48148710586d313c29d823
markusritschel <git@markusritschel.de>
2024-06-11 19:26:27 +0200

    Add history xarray accessor

--- src/my_code_base/core/xarray_utils.py
+++ src/my_code_base/core/xarray_utils.py
@@ -11,3 +11,43 @@ import xarray as xr
 log = logging.getLogger(__name__)
 
 
+@xr.register_dataset_accessor("history")
+class HistoryAccessor:
+    def __init__(self, xr_obj):
+        """
+        A class for accessing and manipulating the 'history' attribute of an xarray object.
+
+        Parameters
+        ----------
+        xr_obj: xarray.Dataset or xarray.DataArray
+            The xarray object to be accessed.
+        """
+        self._obj = xr_obj
+        self._ensure_history()
+
+    def _ensure_history(self):
+        """
+        Ensure that the 'history' attribute exists in the xarray object.
+        If it doesn't exist, create an empty 'history' attribute.
+        """
+        if 'history' not in self._obj.attrs.keys():
+            self._obj.attrs['history'] = ""
+
+    def add(self, msg):
+        """
+        Add an entry to the history.
+
+        Parameters
+        ----------
+        msg: str
+            The message to be added to the history.
+
+        Example
+        -------
+        >>> da.history.add("New entry to history")        # doctest: +SKIP
+        """
+        self._obj.attrs['history'] += msg
+        log.debug("Wrote '%s' to history", msg)
+        return
+
+
