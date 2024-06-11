4dbc609fac687f5680b4cb72a832a87979c3d16b
markusritschel <git@markusritschel.de>
2024-06-11 19:13:15 +0200

    Add function to find the nearest element

--- src/my_code_base/core/utils.py
+++ src/my_code_base/core/utils.py
@@ -9,3 +9,26 @@ import logging
 logging.basicConfig(level="INFO")
 
 log = logging.getLogger(__name__)
+def find_nearest(items: list | np.ndarray, pivot: float) -> float:
+    """
+    Find the element inside `items` that is closest to the `pivot` element.
+
+    Parameters
+    ----------
+    items: 
+        A list of elements to search from.
+    pivot: 
+        The pivot element to find the closest element to.
+
+    Returns
+    -------
+    float
+        The element from `items` that is closest to the `pivot` element.
+
+    Examples
+    --------
+    >>> find_nearest(np.array([2,4,5,7,9,10]), 4.6)
+    5
+    """
+    return min(items, key=lambda x: abs(x - pivot))
+
