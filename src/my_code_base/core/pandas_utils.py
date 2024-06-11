3ca8e09177604528370deca52b961566a1d559f1
markusritschel <git@markusritschel.de>
2024-06-11 19:20:49 +0200

    Add pandas_utils module

---
+++ src/my_code_base/core/pandas_utils.py
@@ -0,0 +1,15 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-06-11
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+import numpy as np
+import pandas as pd
+from .utils import centered_bins
+
+
+log = logging.getLogger(__name__)
+
+
