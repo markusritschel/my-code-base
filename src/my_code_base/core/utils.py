492698c99fe21be9a31c85f2c7ef5e1f83b373f3
markusritschel <git@markusritschel.de>
2024-06-11 16:17:47 +0200

    Add utils.py

---
+++ src/my_code_base/core/utils.py
@@ -0,0 +1,11 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-03
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+logging.basicConfig(level="INFO")
+
+log = logging.getLogger(__name__)
