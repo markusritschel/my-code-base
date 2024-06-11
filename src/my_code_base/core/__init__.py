04a3b6e5b8cdbd871d523349d9c0e422dca87f36
markusritschel <git@markusritschel.de>
2024-06-11 16:21:14 +0200

    Add core module

---
+++ src/my_code_base/core/__init__.py
@@ -0,0 +1,12 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-03
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+"""This module provides a set of core functions"""
+import logging
+
+
+log = logging.getLogger(__name__)
+
