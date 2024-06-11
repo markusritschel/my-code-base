aeef98bd4c2db988a5f1cd0c3124f5bd84bd6529
markusritschel <git@markusritschel.de>
2024-06-11 19:25:30 +0200

    Add xarray_utils module

---
+++ src/my_code_base/core/xarray_utils.py
@@ -0,0 +1,13 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-03
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+import numpy as np
+import xarray as xr
+
+log = logging.getLogger(__name__)
+
+
