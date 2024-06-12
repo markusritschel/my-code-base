2b1c234445e6f9a5d3142ee6905e14c77ade5bc7
markusritschel <git@markusritschel.de>
2024-06-12 03:17:46 +0200

    Add function to convert mole fraction ppm to partial pressure µatm

--- src/my_code_base/ocean/utils.py
+++ src/my_code_base/ocean/utils.py
@@ -92,3 +92,38 @@ def water_vapor_pressure(T, S):
     return pH2O
 
 
+def ppm2uatm(xCO2, p_equ, input='wet', T=None, S=None):
+    """Convert mole fraction concentration (in ppm) into partial pressure (in µatm) following :cite:t:`dickson_guide_2007`
+
+    .. math::
+        pCO_2 = xCO_2 \\cdot p_\\text{equ}
+
+    Parameters
+    ----------
+    xCO2: float or pandas.Series
+        The measured CO2 concentration (in ppm)
+    p_equ: float or pandas.Series
+        The measured pressure (in hPa, Pa or atm) at the equilibrator (hint: you might want to smoothen your time series)
+    input: str [default: "wet"]
+        Either "wet" or "dry", specifying the type of air, in which the concentration is measured.
+        If the CO2 concentration is measured in dry air, one must correct for the water vapor pressure.
+        In this case, make sure to also provide T (temperature in Kelvin) and S (salinity in PSU) as arguments.
+    T: float or pandas.Series [default: None]
+        Temperature in Kelvin (needs to be provided if xCO2 is measured in dry air)
+    S: float or pandas.Series [default: None]
+        Salinity in PSU (needs to be provided if xCO2 is measured in dry air)
+    """
+    # Pa or hPa -> atm
+    p_equ = pressure2atm(p_equ)
+
+    if input == "dry":
+        pH2O = water_vapor_pressure(T, S)
+    elif input == "wet":
+        pH2O = 0
+    else:
+        raise IOError("Input must be either 'dry' or 'wet'.")
+
+    pCO2_wet_equ = xCO2*(p_equ - pH2O)
+
+    return pCO2_wet_equ
+
