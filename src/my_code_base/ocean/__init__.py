3cf95061857ac3a7c10ee622bdf5eeb7b8393aff
markusritschel <git@markusritschel.de>
2024-06-12 03:14:14 +0200

    Add ocean package

---
+++ src/my_code_base/ocean/__init__.py
@@ -0,0 +1,12 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-03
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+"""This module provides routines for tackling tasks related to oceanic biogeochemistry.
+import logging
+
+
+log = logging.getLogger(__name__)
+
