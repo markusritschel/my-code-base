0165807f49330d99545b5c1f85c562daad791226
markusritschel <git@markusritschel.de>
2024-06-12 16:41:44 +0200

    Add colors module

---
+++ src/my_code_base/plot/colors.py
@@ -0,0 +1,11 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-06-08
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
+
