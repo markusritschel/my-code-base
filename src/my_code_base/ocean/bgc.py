77cc33663fd7e84e260e1b93d4ac8f9f46daff13
markusritschel <git@markusritschel.de>
2024-06-12 03:18:20 +0200

    Add ocean.bgc module for biogeochemistry tasks

---
+++ src/my_code_base/ocean/bgc.py
@@ -0,0 +1,11 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-03
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+log = logging.getLogger(__name__)
+
+
