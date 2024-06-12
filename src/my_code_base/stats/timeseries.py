3070cefc60748a77f630363f4b87af34e4e06d74
markusritschel <git@markusritschel.de>
2024-06-12 12:47:54 +0200

    Add function xr_deseasonalize
    Data get first detrended, then the long-term average of every season is subtracted for each season. Finally, the trend is added again.

--- src/my_code_base/stats/timeseries.py
+++ src/my_code_base/stats/timeseries.py
@@ -8,6 +8,7 @@
 #
 import numpy as np
 import xarray as xr
+import xarrayutils
 
 
 def weighted_annual_mean(ds: xr.Dataset | xr.DataArray):
@@ -45,3 +46,26 @@ def weighted_annual_mean(ds: xr.Dataset | xr.DataArray):
 
     output = output.assign_coords(year=('time', output.time.dt.year.values), keep_attrs=True)
     return output
+
+
+def xr_deseasonalize(ds, freq=12, dim='time'):
+    """Remove the seasonal cycle of an :class:`xr.Dataset` object.
+    Data get first detrended, then the long-term average of every season is subtracted for
+    each season. Finally, the trend is added again.
+
+    Parameters
+    ----------
+    freq : int
+        The frequency of the data. Default is 12 for monthly resolution.
+    dim : str
+        The name of the time dimension.
+    """
+    # trend = ds.rolling({dim: freq + 1, 'center': True}).mean(dim=dim)
+    trend = xarrayutils.linear_trend(ds, dim=dim)
+    detrended = ds - trend
+    detrended_deseasonalized = (
+        detrended.groupby(f"{dim}.month") - detrended.groupby(f"{dim}.month").mean()
+    )
+    deseasonalized = detrended_deseasonalized + trend
+    return deseasonalized
+
