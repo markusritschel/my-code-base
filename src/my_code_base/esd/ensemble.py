03c9b843df9bc6eab6e7ada7e821ae80325dde23
markusritschel <git@markusritschel.de>
2024-06-12 02:53:34 +0200

    Add ensemble module

---
+++ src/my_code_base/esd/ensemble.py
@@ -0,0 +1,10 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-26
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
