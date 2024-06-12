ae1324cdd8ea8e865cbe0420b6638d6d483127e9
markusritschel <git@markusritschel.de>
2024-06-12 03:11:10 +0200

    Add linalg package

---
+++ src/my_code_base/linalg/__init__.py
@@ -0,0 +1,10 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-04-04
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
