eaa0662e32b0dc82cec8d5183f50535f8cdeae62
markusritschel <git@markusritschel.de>
2024-06-12 03:21:57 +0200

    Add stats package

---
+++ src/my_code_base/stats/__init__.py
@@ -0,0 +1,11 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  kontakt@markusritschel.de
+# Date:   2024-03-03
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
+
