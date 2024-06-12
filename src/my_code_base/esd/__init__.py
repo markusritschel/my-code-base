ca67be4b6177236dd367663669ba28fab572e8da
markusritschel <git@markusritschel.de>
2024-06-12 02:52:58 +0200

    Add esd package

---
+++ src/my_code_base/esd/__init__.py
@@ -0,0 +1,12 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-03
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+"""A module for helping process Earth System Data"""
+import logging
+
+
+log = logging.getLogger(__name__)
+
