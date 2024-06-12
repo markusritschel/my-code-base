e6203b31e8d178f126ea86de1bb938ffa1de417f
markusritschel <git@markusritschel.de>
2024-06-12 03:20:03 +0200

    Update ocean package

--- src/my_code_base/ocean/__init__.py
+++ src/my_code_base/ocean/__init__.py
@@ -5,8 +5,14 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
 """This module provides routines for tackling tasks related to oceanic biogeochemistry.
+
+- temperature and salinity normalization
+- temperature decomposition
+- fgCO2 <-> pCO2 and other conversions
+"""
 import logging
 
 
 log = logging.getLogger(__name__)
 
+from .utils import *
