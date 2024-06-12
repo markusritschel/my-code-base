a72e0eaedd685d8391e52f9a69d6790428a8c4d7
markusritschel <git@markusritschel.de>
2024-06-12 03:15:03 +0200

    Add ocean.utils module

---
+++ src/my_code_base/ocean/utils.py
@@ -0,0 +1,12 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-03-03
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
