354e2f1ee02b83a87002de093ddc3954f04b4960
markusritschel <git@markusritschel.de>
2024-06-20 17:06:02 +0200

    Add stats.xarray_utils module

---
+++ src/my_code_base/stats/xarray_utils.py
@@ -0,0 +1,12 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-06-19
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
