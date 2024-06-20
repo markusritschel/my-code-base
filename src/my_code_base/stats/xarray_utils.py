320e32a60a13320e5d24a0b4c02d0a8ca6101091
markusritschel <git@markusritschel.de>
2024-06-20 17:12:32 +0200

    Add dispatchable method weighted_mean
    Can accept a single dimension for time average or a tuple of lon/lat dimensions (tbd).

--- src/my_code_base/stats/xarray_utils.py
+++ src/my_code_base/stats/xarray_utils.py
@@ -6,6 +6,8 @@
 #
 import logging
 import xarray as xr
+from multipledispatch import dispatch
+from .timeseries import weighted_annual_mean
 
 
 log = logging.getLogger(__name__)
@@ -22,8 +24,31 @@ class StatsAccessor:
     obj : xarray.Dataset or xarray.DataArray
         The xarray object on which statistical operations will be performed.
 
+    Methods
+    -------
+    weighted_mean(dim)
+        Calculate the weighted mean based on the given dimension.
     """
 
     def __init__(self, obj):
         self._obj = obj
+
+    @dispatch(str)
+    def weighted_mean(self, dim):
+        """
+        Calculate the weighted annual mean (taking days of months into account).
+
+        Parameters
+        ----------
+        dim : str
+            The dimension along which to calculate the weighted mean.
+
+        Returns
+        -------
+        weighted_mean : xarray.Dataset or xarray.DataArray
+            The weighted annual mean.
+
+        """
+        log.debug(f"Identified single dimension {dim}. Building weighted annual mean.")
+        return weighted_annual_mean(self._obj)
 
