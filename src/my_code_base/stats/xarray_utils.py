6f00f81fcd97c0321a7ea8ff775c56ed001b6b92
markusritschel <git@markusritschel.de>
2024-06-20 17:09:27 +0200

    Add StatsAccessor for xarray

--- src/my_code_base/stats/xarray_utils.py
+++ src/my_code_base/stats/xarray_utils.py
@@ -5,8 +5,25 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
 import logging
+import xarray as xr
 
 
 log = logging.getLogger(__name__)
 
+
+@xr.register_dataset_accessor("stats")
+@xr.register_dataarray_accessor("stats")
+class StatsAccessor:
+    """
+    This class provides statistical operations on xarray data.
+
+    Parameters
+    ----------
+    obj : xarray.Dataset or xarray.DataArray
+        The xarray object on which statistical operations will be performed.
+
+    """
+
+    def __init__(self, obj):
+        self._obj = obj
 
